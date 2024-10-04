ea7a74f0640b96f67b6c81231d024d9550eb9590
ctomassetti56 <cjt3870@rit.edu>
2024-10-04 00:23:18 -0400

    make file output more accurate

--- system_report.py
+++ system_report.py
@@ -88,18 +88,40 @@ def memory_information():
 def output_information_to_file():
     with open('system_report.txt', 'w') as f:
         f.write("Device Information:\n")
-        f.write("Hostname:\t\t" + str(platform.node()) + "\n")
-        f.write("Domain:\t\t\t" + str(socket.getfqdn()) + "\n\n")
+        fqdn = socket.getfqdn()
+        hostname = fqdn.split('.')[0]
+        domain = '.'.join(fqdn.split('.')[1:])
+        f.write("Hostname:\t\t" + str(hostname) + "\n")
+        f.write("Domain:\t\t\t" + str(domain) + "\n\n")
+        
         f.write("Network Information:\n")
-        f.write("IP Address:\t\t" + str(socket.gethostbyname(socket.gethostname())) + "\n")
-        f.write("Gateway:\t\t" + str(subprocess.check_output("ip route | grep default | awk '{print $3}'", shell=True).decode().strip()) + "\n")
-        f.write("Network Mask:\t\t" + str(subprocess.check_output("ip route | grep kernel | awk '{print $1}'", shell=True).decode().splitlines()[0].strip()) + "\n")
-        f.write("DNS1:\t\t\t" + str(subprocess.check_output("cat /etc/resolv.conf | grep nameserver | awk '{print $2}'", shell=True).decode().splitlines()[0].strip()) + "\n")
-        f.write("DNS2:\t\t\t" + str(subprocess.check_output("cat /etc/resolv.conf | grep nameserver | awk '{print $3}'", shell=True).decode().strip()) + "\n\n")
+        ip_address = socket.gethostbyname(socket.gethostname())
+        gateway = subprocess.check_output("ip route | grep default | awk '{print $3}'", shell=True).decode().strip()
+        network_mask = subprocess.check_output("ip route | grep kernel | awk '{print $1}'", shell=True).decode().splitlines()[0].strip()
+        dns1 = subprocess.check_output("cat /etc/resolv.conf | grep nameserver | awk '{print $2}'", shell=True).decode().splitlines()[0].strip()
+        dns2 = subprocess.check_output("cat /etc/resolv.conf | grep nameserver | awk '{print $3}'", shell=True).decode().strip()
+        f.write("IP Address:\t\t" + str(ip_address) + "\n")
+        f.write("Gateway:\t\t" + str(gateway) + "\n")
+        f.write("Network Mask:\t\t" + str(network_mask) + "\n")
+        f.write("DNS1:\t\t\t" + str(dns1) + "\n")
+        if dns2:
+            f.write("DNS2:\t\t\t" + str(dns2) + "\n\n")
+        else:
+            f.write("DNS2:\t\t\tN/A\n\n")
+        
         f.write("System Information:\n")
-        f.write("Operating System:\t" + str(platform.system()) + "\n")
-        f.write("OS Version:\t\t" + str(platform.release()) + "\n")
-        f.write("Kernel Version:\t\t" + str(subprocess.check_output('uname -r', shell=True).decode().strip()) + "\n\n")
+        os_info = {}
+        with open('/etc/os-release') as f_os:
+            for line in f_os:
+                key, value = line.strip().split('=', 1)
+                os_info[key] = value.strip('"')
+        operating_system = os_info.get('PRETTY_NAME', 'Unknown OS')
+        os_version = os_info.get('VERSION', 'Unknown Version')
+        kernel_version = subprocess.check_output('uname -r', shell=True).decode().strip()
+        f.write("Operating System:\t" + str(operating_system) + "\n")
+        f.write("OS Version:\t\t" + str(os_version) + "\n")
+        f.write("Kernel Version:\t\t" + str(kernel_version) + "\n\n")
+        
         f.write("Storage Information:\n")
         try:
             root_fs_line = subprocess.check_output("df -h | grep ' /$'", shell=True).decode().strip()
@@ -111,14 +133,22 @@ def output_information_to_file():
             available_space = "N/A"
         f.write("Hard Drive Capacity:\t" + str(hard_drive_capacity) + "\n")
         f.write("Available Space:\t" + str(available_space) + "\n\n")
+        
         f.write("Processor Information:\n")
-        f.write("CPU Model:\t\t" + str(subprocess.check_output("cat /proc/cpuinfo | grep 'model name' | uniq | awk '{print $4, $5, $6, $7, $8, $9}'", shell=True).decode().strip()) + "\n")
-        f.write("Number of Processors:\t" + str(subprocess.check_output("cat /proc/cpuinfo | grep processor | wc -l", shell=True).decode().strip()) + "\n")
-        f.write("Number of Cores:\t" + str(subprocess.check_output("cat /proc/cpuinfo | grep 'cpu cores' | uniq | awk '{print $4}'", shell=True).decode().strip()) + "\n\n")
+        cpu_model = subprocess.check_output("cat /proc/cpuinfo | grep 'model name' | uniq | awk '{print $4, $5, $6, $7, $8, $9}'", shell=True).decode().strip()
+        num_of_processors = subprocess.check_output("cat /proc/cpuinfo | grep processor | wc -l", shell=True).decode().strip()
+        num_of_cores = subprocess.check_output("cat /proc/cpuinfo | grep 'cpu cores' | uniq | awk '{print $4}'", shell=True).decode().strip()
+        f.write("CPU Model:\t\t" + str(cpu_model) + "\n")
+        f.write("Number of Processors:\t" + str(num_of_processors) + "\n")
+        f.write("Number of Cores:\t" + str(num_of_cores) + "\n\n")
+        
         f.write("Memory Information:\n")
-        f.write("Total RAM:\t\t" + str(subprocess.check_output("free -h | grep Mem | awk '{print $2}'", shell=True).decode().strip()) + "\n")
-        f.write("Available RAM:\t\t" + str(subprocess.check_output("free -h | grep Mem | awk '{print $7}'", shell=True).decode().strip()) + "\n")
-        subprocess.run(["xdg-open", "system_report.txt"])
+        total_ram = subprocess.check_output("free -h | grep Mem | awk '{print $2}'", shell=True).decode().strip()
+        available_ram = subprocess.check_output("free -h | grep Mem | awk '{print $7}'", shell=True).decode().strip()
+        f.write("Total RAM:\t\t" + str(total_ram) + "\n")
+        f.write("Available RAM:\t\t" + str(available_ram) + "\n")
+    
+    subprocess.run(["xdg-open", "system_report.txt"])
 
 def main():
     device_information()
