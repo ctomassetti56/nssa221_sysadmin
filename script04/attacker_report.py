331b190cba7e4d25704ed9e9770e15e4b3ddb738
ctomassetti56 <cjt3870@rit.edu>
2024-11-08 11:25:40 -0500

    format

--- script04/attacker_report.py
+++ script04/attacker_report.py
@@ -83,13 +83,13 @@ def main():
     read_log()
     #sort the dictionary by value
     sorted_attempts = dict(sorted(failed_attempts.items(), key=lambda item: item[1], reverse=False))
-    print("IP Address\t\t  Failed Attempts\t\tCountry")
+    print("  IP Address\t\t Failed Attempts\t\tCountry")
     print("---------------------------------------------------------")
     for ip, count in sorted_attempts.items():
         if count >= failed_attempts_allowed:
             match = geolite2.lookup(ip)
             country = match.country if match else "Unknown"
-            print(f"{ip}\t\t\t{count}\t\t\t{country}")
+            print(f"{ip}\t\t\t{count}\t\t\t {country}")
 
 if __name__ == "__main__":
     main()
